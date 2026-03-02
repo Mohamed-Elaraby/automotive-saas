a3e5976b8b977ba6f772a1a32ae09e8ae257812f
Mohamed New Lap Dac <mido.b333@gmail.com>
2026-03-02 16:36:26 +0400

    fix: tenant model parse error

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -1,7 +1,5 @@
 <?php
 
-<?php
-
 namespace App\Models;
 
 use Stancl\Tenancy\Contracts\TenantWithDatabase;
@@ -10,20 +8,14 @@ use Stancl\Tenancy\DatabaseConfig;
 
 class Tenant extends BaseTenant implements TenantWithDatabase
 {
-    /**
-     * Tell stancl where the tenant database name is stored.
-     * We'll store it in the "data" JSON column.
-     */
     public function database(): DatabaseConfig
     {
-        // Pick a db name, store it once, then reuse it forever.
         $dbName = $this->getInternal('db_name');
 
         if (! $dbName) {
             $dbName = 'tenant_' . $this->id;
-            $this->setInternal('db_name', $dbName);
 
-            // Important: persist on central DB
+            $this->setInternal('db_name', $dbName);
             $this->save();
         }
 
