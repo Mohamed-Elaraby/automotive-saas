60ecb66f306749be5c9b4db5f72c2e747d76b3bb
Mohamed New Lap Dac <mido.b333@gmail.com>
2026-03-02 16:22:09 +0400

    fix: revert Tenant model override, use base stancl tenant

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -2,19 +2,9 @@
 
 namespace App\Models;
 
-use Stancl\Tenancy\Contracts\TenantWithDatabase;
 use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
-use Stancl\Tenancy\DatabaseConfig;
 
-class Tenant extends BaseTenant implements TenantWithDatabase
+class Tenant extends BaseTenant
 {
-    public function database(): DatabaseConfig
-    {
-        $config = new DatabaseConfig($this);
-
-        // customize database name
-        $config->setName('tenant_' . $this->id);
-
-        return $config;
-    }
+    //
 }
