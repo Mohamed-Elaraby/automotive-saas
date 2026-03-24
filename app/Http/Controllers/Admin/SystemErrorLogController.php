8f1f7bd0ef34f26bc0e395cecd4258f332c490d5
Mohamed New Lap Dac <mido.b333@gmail.com>
2026-03-24 12:27:01 +0400

    Fix parse error in system error log controller

--- app/Http/Controllers/Admin/SystemErrorLogController.php
+++ app/Http/Controllers/Admin/SystemErrorLogController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models<SystemErrorLog;
+use App\Models\SystemErrorLog;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Schema;
